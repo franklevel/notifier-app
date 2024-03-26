38ffb864b091764eb8a45fe719d529b9a0639ffb
Frank Ruiz <franklevel@gmail.com>
2024-03-26 15:32:32 -0300

    chore: remove app file

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
